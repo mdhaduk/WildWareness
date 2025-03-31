25183931867115a2d2d2b2bd1de7f4a50f09bd30
mdhaduk <mdhaduk7@gmail.com>
2025-03-31 11:38:50 -0500

    .

--- backend/database.py
+++ backend/database.py
@@ -7,8 +7,10 @@ import requests
 from scripts.helper_scripts import get_county_from_address
 from models import Base, Wildfire, Shelter, NewsReport, TESTING
 from dotenv import load_dotenv
+from pathlib import Path
 from collections import defaultdict
-load_dotenv()
+env_path = Path("/Users/milandhaduk/CS373/cs373-spring-2025-group-03/.env")
+load_dotenv(dotenv_path=env_path)
 DATABASE_URL = ""
 if TESTING:
     DATABASE_URL = "sqlite:///test.db"
