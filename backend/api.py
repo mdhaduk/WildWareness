a4cb930c992966cf7af7cfd2abf5df6f21f8b7d7
PoojaVasanthan <poojavasanthan10@gmail.com>
2025-04-02 17:34:32 -0500

    Filtering + Sorting for News Reports

--- backend/api.py
+++ backend/api.py
@@ -6,13 +6,13 @@ from models import Wildfire, Shelter, NewsReport
 from models import TESTING
 from scripts import ca_fire_gov
 from flask import render_template_string
+from dotenv import load_dotenv
 import os
 import awsgi
 from datetime import datetime
-
+load_dotenv()
 
 app = Flask(__name__)
-# app.config["DEBUG"] = True
 app.json.sort_keys = False
 CORS(app)
 
@@ -22,19 +22,47 @@ if TESTING:
 else:
     username = os.getenv("DB_USERNAME")
     password = os.getenv("DB_PASSWORD")
-    database_name = "homelessaid"
-    DATABASE_URL = f"postgresql+psycopg2://{username}:{password}@homelessaid-database.ctc8886awsgr.us-east-2.rds.amazonaws.com:5432/{database_name}"
+    database_name = "wildfiredb"
+    DATABASE_URL = f"postgresql+psycopg2://{username}:{password}@wildwarenessdb.czwce00s2t3z.us-east-2.rds.amazonaws.com/{database_name}"
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 local_session = sessionmaker(bind=engine, autoflush=False, future=True)
 
 DEFAULT_PAGE_SIZE = 10
 
+def searchModels(search_text, query, MODEL):
+    phrase_tsquery = func.plainto_tsquery('english', search_text)
+    words_tsquery_input = ' | '.join(search_text.split())
+    words_tsquery = func.to_tsquery('english', words_tsquery_input)
+    conditions = []
+    columns = ['name', 'description', 'city', 'target', 'eligibility', 'counties_served', 'main_services', 'detailed_target', 'detailed_hours', 'operating_hours']
+    for column_name in columns:
+        if not hasattr(MODEL, column_name):
+            continue
+        column = getattr(MODEL, column_name)      
+        phrase_match = func.to_tsvector('english', column).op('@@')(phrase_tsquery)
+        words_match = func.to_tsvector('english', column).op('@@')(words_tsquery)
+        conditions.append(or_(phrase_match, words_match))
+
+    # Combine all conditions with OR, so any match on any specified field will be included
+    if conditions:
+        query = query.filter(or_(*conditions))
+    
+    return query
 
 @app.route("/wildfire_incidents", methods=["GET"])
 def get_all_incidents():
     page = request.args.get("page", 1, type=int)
     size = request.args.get("size", DEFAULT_PAGE_SIZE, type=int)
+    sort_by = request.args.get('sort_by', 'city')
+    order = request.args.get('order', 'asc')
+    hours = request.args.get('hours', None)
+    county = request.args.get('county', None)
+    search = request.args.get('search', None)
+    eligibility = request.args.get('eligibility' , None)
+    valid_sort_columns = {'name', 'city'}
+    if sort_by not in valid_sort_columns:
+        return jsonify({'error': f"Invalid sort column '{sort_by}'"}), 400
     with local_session() as ls:
         try:
             incidents = ls.query(Wildfire).limit(size).offset((page - 1) * size).all()
@@ -71,7 +99,6 @@ def get_single_incident(id):
 def get_all_shelters():
     page = request.args.get("page", 1, type=int)
     size = request.args.get("size", DEFAULT_PAGE_SIZE, type=int)
-
     with local_session() as ls:
         try:
             shelters = ls.query(Shelter).limit(size).offset((page - 1) * size).all()
@@ -108,62 +135,93 @@ def get_single_shelter(id):
 @app.route("/news", methods=["GET"])
 def get_all_reports():
     page = request.args.get("page", 1, type=int)
-    size = request.args.get("size", DEFAULT_PAGE_SIZE, type=int)
-
-    # sort_by = request.args.get("sortBy", "title")  # Default sorting by name
-    # order = request.args.get("order", "asc")  # Default ordering by ascending
+    size = request.args.get("size", 2, type=int)
     source = request.args.get("source", None)
-    # author = request.args.get("author", None)
-    # date = request.args.get("date", None)
+    author = request.args.get("author", None)
+    date = request.args.get("date", None)
+    categories = request.args.get('categories', '') 
+    sortBy = request.args.get("sortBy", "title")  # Default to 'title'
+    order = request.args.get("order", "asc")  # Default to ascending order
 
+    # Trim whitespace from the source
+    if source:
+        source = source.strip()
+    if author:
+        author = author.strip()
+    # category_list = categories.split(',')
 
     with local_session() as ls:
+        query = ls.query(NewsReport)
+
+        # Apply source filtering
+        if source:
+            query = query.filter(func.lower(NewsReport.source) == source.lower())
+        
+        if author:
+            query = query.filter(func.lower(NewsReport.author) == author.lower())
+
+        if date:
+            # Ensure the date is in the 'YYYY-MM-DD' format
+            date_obj = datetime.strptime(date, '%Y-%m-%d').date()
+            # Convert the date to a string format that matches the `published_at` field
+            date_string = date_obj.strftime('%Y-%m-%d')
+            # Apply the filter to match the string representation of the date
+            query = query.filter(NewsReport.published_at == date_string)
+
+        if categories:
+            category_list = categories.split(',')  # Split categories into a list
+            # Apply OR condition (LIKE operator for each selected category)
+            for category in category_list:
+                query = query.filter(NewsReport.categories.like(f"%{category}%"))
+
+        # # Sorting logic
+        # If sorting by date, convert to date and apply sorting
+        if sortBy == "date":
+            if order == "asc":
+                query = query.order_by(func.to_date(NewsReport.published_at, 'YYYY-MM-DD').asc())
+            else:
+                query = query.order_by(func.to_date(NewsReport.published_at, 'YYYY-MM-DD').desc())
+
+        else:
+            if order == "asc":
+                query = query.order_by(getattr(NewsReport, sortBy))
+            else:
+                query = query.order_by(getattr(NewsReport, sortBy).desc())
+
+        # if sort_by == "date":
+        #     if order == "desc":
+        #         # Sort by date, assuming it's in the correct format (YYYY-MM-DD)
+        #         query = query.order_by(func.trim(NewsReport.published_at).desc())  # Trim whitespace before sorting
+        #     else:
+        #         query = query.order_by(func.trim(NewsReport.published_at).asc())  # Trim whitespace before sorting
+        # elif sort_by == "title":
+        #     if order == "asc":
+        #         # Sort by title, applying trim to remove whitespace from the beginning and end
+        #         query = query.order_by(func.trim(NewsReport.title).lower().asc())  # Trim whitespace and sort lexicographically
+        #     else:
+        #         query = query.order_by(func.trim(NewsReport.title).lower().desc())  # Trim whitespace and sort in reverse order
+
+
+
         try:
-            query = ls.query(NewsReport)
-            print(source)
-            if source:
-                query = query.filter(func.lower(NewsReport.source) == source.lower())
+            reports = query.limit(size).offset((page - 1) * size).all()  # use query instead of directly querying
+            report_cards = [report.as_instance() for report in reports]
+            total_reports = query.count()  # Using filtered query for counting
+            total_pages = (total_reports + size - 1) // size
 
-
-            # if author:
-            #     query = query.filter(NewsReport.author.ilike(f"%{author}%"))
-
-            # if date:
-            #     date_obj = datetime.strptime(date, '%Y-%m-%d')
-            #     query = query.filter(NewsReport.published_at == date_obj)
-
-
-            # Sorting logic
-            # if sort_by == "date":
-            #     if order == "desc":
-            #         query = query.order_by(NewsReport.published_at.desc())
-            #     else:
-            #         query = query.order_by(NewsReport.published_at.asc())
-            # elif sort_by == "title":
-            #     if order == "asc":
-            #         query = query.order_by(NewsReport.title.lower().asc())
-            #     else:
-            #         query = query.order_by(NewsReport.title.lower().desc())
-
-
-            incidents = query.limit(size).offset((page - 1) * size).all()
-            print(incidents)
-            incident_cards = [incident.as_instance() for incident in incidents]
-            total_incidents = query.count()
-            total_pages = (total_incidents + size - 1) // size
             return jsonify(
                 {
-                    "incidents": incident_cards,
+                    "incidents": report_cards,
                     "pagination": {
                         "page": page,
                         "size": size,
                         "total_pages": total_pages,
-                        "total_items": total_incidents,
+                        "total_items": total_reports,
                     },
                 }
             )
-        except:
-            return jsonify({"error": "issue getting data"}), 500
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
 
 
 @app.route("/news/<int:id>", methods=["GET"])
@@ -180,4 +238,4 @@ def get_single_report(id):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=3000)
--- frontend/src/pages/NewsReports.jsx
+++ frontend/src/pages/NewsReports.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useSearchParams } from 'react-router-dom';
 import Pagination from '../components/Pagination';
+import { useNavigate } from 'react-router-dom';
 
 function NewsReports() {
     const [reports, setReports] = useState([]);
@@ -12,6 +13,7 @@ function NewsReports() {
     const [loading, setLoading] = useState(false); // Loading state to handle loading spinner
     const [error, setError] = useState(null);
     const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
 
     // Sorting and Filtering States
     const [sortBy, setSortBy] = useState('title');
@@ -19,6 +21,7 @@ function NewsReports() {
     const [source, setSource] = useState('');
     const [author, setAuthor] = useState('');
     const [date, setDate] = useState('');
+    const [categories, setCategories] = useState([]);
 
     // Extract page parameter from URL
     const pageParam = parseInt(searchParams.get("page")) || 1;
@@ -26,7 +29,6 @@ function NewsReports() {
 
     // Fetch reports from the API with the current filters applied
     useEffect(() => {
-        console.log("Working")
         const fetchReports = async () => {
             setLoading(true); // Start loading before fetching data
             try {
@@ -35,8 +37,14 @@ function NewsReports() {
                     page: pageParam,
                     size: itemsPerPage,
                     source: source,
+                    author: author,
+                    date: date,
+                    categories: categories.join(','),
+                    sortBy: sortBy, 
+                    order: order
                 };
-                const response = await axios.get(`http://localhost:5000/news`, { params });
+                // This port has to match the one in api.py
+                const response = await axios.get(`http://localhost:3000/news`, { params });
 
                 console.log(response.data); // Debugging
                 setReports(response.data.incidents); // Update reports to the filtered results
@@ -52,7 +60,7 @@ function NewsReports() {
         };
 
         fetchReports();
-    }, [pageParam, itemsPerPage, source]); // Dependencies to trigger refetch when state changes
+    }, [pageParam, source, author, date, categories, sortBy, order]); // Dependencies to trigger refetch when state changes
 
     // // Update URL based on filters and page
     // useEffect(() => {
@@ -74,49 +82,81 @@ function NewsReports() {
 
     // Handle filter change and update corresponding state
     const handleFilterChange = (e) => {
-        const { name, value } = e.target;
-        // if (name === 'sortBy') setSortBy(value);
-        // else if (name === 'order') setOrder(value);
+        const { name, value, checked} = e.target;
         if (name === 'source') setSource(value);
-        
-        // else if (name === 'author') setAuthor(value);
-        // else if (name === 'date') setDate(value);
-        // fetchReports();
+        else if (name === 'author') setAuthor(value);
+        else if (name === 'date') setDate(value);
+        else if (name === 'categories') {
+            setCategories((prevCategories) => {
+                if (checked) {
+                    // Add category if checked
+                    return [...prevCategories, value];
+                } else {
+                    // Remove category if unchecked
+                    return prevCategories.filter((category) => category !== value);
+                }
+            });
+        }
+        else if (name === 'sortBy') setSortBy(value);
+        else if (name === 'order') setOrder(value);
+
+        // Reset the page to 1 whenever any filter is changed
+        setSearchParams({
+            page: 1, // Always reset to page 1
+            source: source,
+            author: author,
+            date: date,
+            categories: categories.join(','), 
+            sortBy: sortBy, 
+            order: order,
+        });
     };
 
+    // Clear all filters and reset to initial state
+    const handleClearFilters = () => {
+        setSource('');
+        setAuthor('');
+        setDate('');
+        setCategories([]);  // Reset categories filter
+        setSortBy('title')
+        setOrder('asc')
+        setSearchParams({
+            page: 1,  // Reset to page 1 after clearing filters
+        });
+    };
     // Trigger a filter update when the user clicks the "Apply Filter" button
     // const handleFilterClick = () => {
-    //     // setReports([]);
-    //     // navigate("/news?page=1");
-    //     // Update searchParams when user clicks "Apply Filter"
-    //     setSearchParams({
-    //         page: 1,
-    //         // sortBy: sortBy,
-    //         // order: order,
-    //         source: source,
-    //         // author: author,
-    //         // date: date,
-    //     });
+        // setReports([]);
+        // navigate("/news?page=1");
+        // Update searchParams when user clicks "Apply Filter"
+        // setSearchParams({
+        //     page: 1,
+        //     // sortBy: sortBy,
+        //     // order: order,
+        //     source: source,
+        //     author: author,
+        //     date: date,
+        // });
        
-    //     // const params = {
-    //     //     page: 1,
-    //     //     size: itemsPerPage,
-    //     //     sortBy: sortBy,
-    //     //     order: order,
-    //     //     source: source,
-    //     //     author: author,
-    //     //     date: date,
-    //     // };
-    //     // try {
-    //     //     const response = axios.get(`https://api.wildwareness.net/news`, { params });
-    //     //     setReports(response.data.incidents); // Update reports to the filtered results
-    //     //     setTotalPages(response.data.pagination.total_pages);
-    //     //     setTotalItems(response.data.pagination.total_items);
-    //     //     setItemsPerPage(response.data.pagination.size);
-    //     // } catch (error) {
-    //     //     console.error("Error fetching news reports:", error);
-    //     //     setError(error);
-    //     // }
+        // const params = {
+        //     page: 1,
+        //     size: itemsPerPage,
+        //     sortBy: sortBy,
+        //     order: order,
+        //     source: source,
+        //     author: author,
+        //     date: date,
+        // };
+        // try {
+        //     const response = axios.get(`https://api.wildwareness.net/news`, { params });
+        //     setReports(response.data.incidents); // Update reports to the filtered results
+        //     setTotalPages(response.data.pagination.total_pages);
+        //     setTotalItems(response.data.pagination.total_items);
+        //     setItemsPerPage(response.data.pagination.size);
+        // } catch (error) {
+        //     console.error("Error fetching news reports:", error);
+        //     setError(error);
+        // }
     // };
 
     return (
@@ -124,26 +164,18 @@ function NewsReports() {
             <h2 className="text-center my-4">News Reports</h2>
 
             {/* Filter Inputs */}
-            {/* <div className="form-group me-2">
-                <label>Source: </label>
+            <div className="form-group me-2">
+                <label>Source:</label>
                 <input
                     type="text"
                     name="source"
                     value={source}
                     onChange={handleFilterChange}
                     className="form-control form-control-sm"
-                    placeholder="Filter by source"
+                    placeholder="Enter Source"
                 />
-            </div> */}
+                </div>
 
-            <div className="form-group me-2">
-                <label>Source: </label>
-                <select name="source" value={source} onChange={handleFilterChange} className="form-control form-control-sm">
-                    <option value="india.com">india.com</option>
-                    <option value="date">Date</option>
-                </select>
-            </div>
-{/* 
             <div className="form-group me-2">
                 <label>Author: </label>
                 <input
@@ -159,7 +191,7 @@ function NewsReports() {
             <div className="form-group me-2">
                 <label>Date: </label>
                 <input
-                    type="text"
+                    type="date"
                     name="date"
                     value={date}
                     placeholder="Filter by date"
@@ -168,26 +200,67 @@ function NewsReports() {
                 />
             </div>
 
-            <div className="form-group me-2">
-                <label>Sort By: </label>
-                <select name="sortBy" value={sortBy} onChange={handleFilterChange} className="form-control form-control-sm">
-                    <option value="title">Title</option>
-                    <option value="date">Date</option>
-                </select>
+        {/* Category Filter - Multi-Select Checkboxes */}
+        <div className="form-group me-2">
+            <label>Categories:</label>
+            <div>
+                {['general', 'business', 'politics', 'food', 'health', 'travel', 'sports', 'tech', 'science'].map((category) => (
+                    <label key={category} className="me-2">
+                        <input
+                            type="checkbox"
+                            name="categories"
+                            value={category}
+                            checked={categories.includes(category)}  // Ensure the checkbox reflects the selection state
+                            onChange={handleFilterChange}  // Handle checkbox state change
+                        />
+                        {category}
+                    </label>
+                ))}
             </div>
+        </div>
 
-            <div className="form-group me-2">
-                <label>Order: </label>
-                <select name="order" value={order} onChange={handleFilterChange} className="form-control form-control-sm">
-                    <option value="asc">Ascending</option>
-                    <option value="desc">Descending</option>
+
+        <div className="form-group me-2">
+            <label>Sort By: </label>
+            <select name="sortBy" value={sortBy} onChange={handleFilterChange} className="form-control form-control-sm">
+                <option value="title">Title</option>
+                <option value="date">Date</option>
+                <option value="author">Author</option>
+                <option value="source">Source</option>
+            </select>
+        </div>
+
+        <div className="form-group me-2">
+            <label>Order: </label>
+            <select name="order" value={order} onChange={handleFilterChange} className="form-control form-control-sm">
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+            </select>
+        </div>
+
+            {/* Filter Mode (Any or All) */}
+            {/* <div className="form-group me-2">
+                <label>Filter Mode:</label>
+                <select
+                    value={filterMode}
+                    onChange={(e) => setFilterMode(e.target.value)}
+                    className="form-control form-control-sm"
+                >
+                    <option value="any">Any of the Selected Categories</option>
+                    <option value="all">All of the Selected Categories</option>
                 </select>
             </div> */}
-
             {/* Apply Filter Button */}
             {/* <button onClick={handleFilterClick} className="btn btn-primary my-3">
                 Apply Filter
             </button> */}
+            
+
+
+            {/* Clear Filters Button */}
+            <button onClick={handleClearFilters} className="btn btn-secondary my-3">
+                Clear Filters
+            </button>
 
             {/* Loading Indicator */}
             {loading && <p className="text-center">Loading...</p>}
